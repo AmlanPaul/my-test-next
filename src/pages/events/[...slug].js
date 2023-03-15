5a177bd43f2de8f54435fb2a6be1129a5ad64ab5
AmlanPaul <111192237+AmlanPaul@users.noreply.github.com>
2023-03-15 13:18:00 +0530

    Update the APp

--- src/pages/events/[...slug].js
+++ src/pages/events/[...slug].js
@@ -1,11 +1,84 @@
 import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../../dummy-data";
+import EventList from "../../components/event-list";
+import Head from "next/head";
+import ResultsTitle from "../../components/results-title";
+import { Fragment } from "react";
+import Button from "../../components/UI/button";
+import ErrorAlert from "../../components/error-alert";
 
 function EventFilterPage() {
   const routes = useRouter();
-  console.log(routes.query);
+  const filterData = routes.query.slug;
+
+  if (!filterData) {
+    return (
+      <Fragment>
+        <Head>
+          <title>Filtered Events </title>
+          <meta name="description" content="Filtered events - Next Js" />
+        </Head>
+        <p className="center">Loading ...</p>
+      </Fragment>
+    );
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    numYear < 2020 ||
+    numYear > 2023
+  ) {
+    return (
+      <Fragment>
+        <Head>
+          <title>Filtered Events </title>
+          <meta name="description" content="Filtered events - Next Js" />
+        </Head>
+        <div className="center">
+          <ErrorAlert><p className="center">Invalid filter inputs , Please check !!!</p></ErrorAlert>
+          <Button link="/events">All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  const date = new Date(numYear, numMonth - 1);
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <Head>
+          <title>Filtered Events </title>
+          <meta name="description" content="Filtered events - Next Js" />
+        </Head>
+        <ResultsTitle date={date} />
+        <p className="center">No events found with the matching filter !</p>
+      </>
+    );
+  }
+
   return (
     <>
-      <div>This is Event Filter Page</div>
+      <Head>
+        <title>Filtered Events </title>
+        <meta name="description" content="Filtered events - Next Js" />
+      </Head>
+      <ResultsTitle date={date} />
+      <EventList iteams={filteredEvents} />
     </>
   );
 }
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,17 +1,29 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { getAllEvents } from "../../../dummy-data";
+import EventList from "../../components/event-list";
+import EventSearch from "../../components/event-search";
+import { Fragment } from "react";
 
 function AllEvents() {
+  const router = useRouter();
+  const allEvents = getAllEvents();
+
+  function findEventHandeler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
-    <>
+    <Fragment>
       <Head>
         <title>All Events </title>
-        <meta
-          name="description"
-          content="All the events - Next Js"
-        />
+        <meta name="description" content="All the events - Next Js" />
       </Head>
-      <div>This is Event List page</div>
-    </>
+      <div>
+        <EventSearch onSearch={findEventHandeler}/>
+        <EventList iteams={allEvents} />
+      </div>
+    </Fragment>
   );
 }
 
