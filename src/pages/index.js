289ea783c09c6e7d147841f7643eb750b2beff8d
Amlan Paul <amlan.paul1@gmail.com>
2023-03-16 17:18:37 +0530

    upadte

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,36 @@
 import { getFeaturedEvents } from "../../dummy-data";
 import EventList from "../components/event-list";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const featureEvents = getFeaturedEvents();
+  const [isLoading,setIsLoading] = useState(true);
+  const [data,setData] = useState([]);
+
+  useEffect(() =>{
+    fetch(
+      "https://nextjsevent-project-default-rtdb.firebaseio.com/events.json"
+    ).then(res =>{
+      return res.json();
+    }).then((data) =>{
+      setIsLoading(false);
+
+      const events =[];
+      for (const key in data) {
+        const event = {
+          ...data[key]
+        }
+
+        events.push(event);
+      }
+      setData(events);
+    })
+  }, [] )
+  if (isLoading) {
+    return <> <p className="center"> Loading ...</p></>
+  }
+  
   return (
     <>
       <Head>
@@ -13,7 +40,7 @@ export default function Home() {
           content="All the featured events for home page - Next Js"
         />
       </Head>
-      <EventList iteams={featureEvents} />
+      <EventList iteams={data} />
     </>
   );
 }
